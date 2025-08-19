ce0292a279870066d41f358000f63641af9d771c
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-19 23:59:48 +0900

    add sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
 // const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+import sampleData from "@/db/sample-data"
+import ProductList from "@/components/shared/product/product-list"
 
-const Homepage = async() => {
+const Homepage =() => {
   // await delay(2000); // Simulate a delay of 2 second for test
   return (
-    <div className=" text-6xl flex min-h-screen flex-col items-center justify-between p-24 text-amber-600">Homepage</div>
+    <>
+    <ProductList data={sampleData.products} title='Newest Arrivals' />
+    </>
   )
 }
 
