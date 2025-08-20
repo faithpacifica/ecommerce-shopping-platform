3d8dcfd5731423448eaf1b4bd89b8be8cae5fc1f
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-20 12:57:38 +0900

    create product card and product-list components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,18 @@
 // const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-import sampleData from "@/db/sample-data"
-import ProductList from "@/components/shared/product/product-list"
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 
-const Homepage =() => {
-  // await delay(2000); // Simulate a delay of 2 second for test
-  return (
-    <>
-    <ProductList data={sampleData.products} title='Newest Arrivals' />
-    </>
-  )
-}
+const Homepage = () => {
+	// await delay(2000); // Simulate a delay of 2 second for test
+	return (
+		<>
+			<ProductList
+				data={sampleData.products}
+				title="Newest Arrivals"
+				limit={4}
+			/>
+		</>
+	);
+};
 
-export default Homepage
+export default Homepage;
