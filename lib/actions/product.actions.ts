23f84f1c12d8ffdb5cf7fb0ae41403e1e72e5fa1
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-26 00:40:19 +0900

    commet 2 lines in product.actions.ts so that make app to work

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,13 @@
 'use server';
 
-// import { PrismaClient } from '@prisma/client';
-import { prisma } from '@/db/prisma';
+import { PrismaClient } from '@prisma/client';//uchirsam error bervotti shunga yoqdim
+// import { prisma } from '@/db/prisma'; //buni ishlatganda error bervotti
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // get latest products
 export async function getLatestProducts() {
-	// const prisma = new PrismaClient();
+	const prisma = new PrismaClient();//uchirsam error bervotti shunga yoqdim
 
 	const data = await prisma.product.findMany({
 		take: LATEST_PRODUCTS_LIMIT,
