f6c4284ae684bc4100a80e6d38c5dd7bca097b85
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-25 23:59:07 +0900

    add neon config for serverless deployment

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod';
 import { formatNumberWithDecimal } from './utils';
+
 const currency = z
   .string()
   .refine(
     (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
     { message: "Price must have exactly two decimal places" }
   )
-// Schema for inserting products
 
+
+// Schema for inserting products
 export const insertProductSchema = z.object({
 	name: z.string().min(3, 'Name must be at least 3 characters'),
 	slug: z.string().min(3, 'Slug must be at least 3 characters'),
@@ -17,6 +19,6 @@ export const insertProductSchema = z.object({
   stock:z.coerce.number(),
   images:z.array(z.string()).min(1,'At least one image is required'),
   isFeatured:z.boolean(),
-  banner:z.string().nullable(),
+  banner:z.string().nullable(), //optional
   price: currency,
 });
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,8 @@
-import {z} from 'zod';
+import { z } from 'zod';
 import { insertProductSchema } from '@/lib/validators';
 
-export type Product =z.infer<typeof insertProductSchema> & {
-  id:string,
-  rating:string,
-  createdAt:Date,
-}
+export type Product = z.infer<typeof insertProductSchema> & {
+	id: string;
+	rating: string;
+	createdAt: Date;
+};
