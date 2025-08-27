07fa1cfe07b209e46f2496d2b6b880c5c24a758c
Dildora Muslimova <faithpacifica@yahoo.com>
2025-08-27 23:41:26 +0900

    add product images ui

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { notFound } from 'next/navigation';
 import ProductPrice from '@/components/shared/product/product-price';
+import ProductImages from '@/components/shared/product/product-images';
 
 const ProductDetailsPage = async (props: {
 	params: Promise<{ slug: string }>;
@@ -15,33 +16,37 @@ const ProductDetailsPage = async (props: {
 
 	return (
 		<section>
-			<div className="grid grid-cols-1 md:grid-cols-5"></div>
-			{/* Images Column */}
+			<div className="grid grid-cols-1 md:grid-cols-5">
+        
+				{/* Images Column */}
+				<div className="col-span-2">
+					<ProductImages images={product.images} />
+				</div>
 
-			<div className="col-span-2">{/* Images Component */}</div>
+				{/* Details Column */}
+				<div className="col-span-2 p-5">
+					<div className="flex flex-col gap-6">
+						<p>
+							{product.brand} / {product.category}
+						</p>
+						<h1 className="h3-bold">{product.name}</h1>
+						<p>
+							{product.rating} of {product.numReviews} Reviews
+						</p>
 
-			{/* Details Column */}
-			<div className="col-span-2 p-5">
-				<div className="flex flex-col gap-6">
-					<p>
-						{product.brand} / {product.category}
-					</p>
-					<h1 className="h3-bold">{product.name}</h1>
-					<p>
-						{product.rating} of {product.numReviews} Reviews
-					</p>
-
-					<div className="flex flex-col sm:flex-row sm:items-center gap-3">
-						<ProductPrice
-							className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
-							value={Number(product.price)}
-						/>
-					</div>
-					<div className="mt-10">
-						<p className="font-semibold">Description</p>
-						<p>{product.description}</p>
+						<div className="flex flex-col sm:flex-row sm:items-center gap-3">
+							<ProductPrice
+								className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+								value={Number(product.price)}
+							/>
+						</div>
+						<div className="mt-10">
+							<p className="font-semibold">Description</p>
+							<p>{product.description}</p>
+						</div>
 					</div>
 				</div>
+
 				{/* Action Column */}
 				<div>
 					<Card>
